1dbf82ffaf4d8d3803e604f7e413e880a5f21dd8
makwana hitesh <makwanah538@gmail.com>
2025-11-03 05:48:03 +0000

    Added git-day-3 folder to main repository

---
+++ git-day-3/main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "my" {
+  
+    ami           = "ami-01760eea5c574eb86"
+    instance_type = var.instance_type
+    key_name = "mykey" 
+    subnet_id = "subnet-048962092a00fa9de"
+    
+    tags = {
+        Name = "MyFirstInstance"
+    }
+}
