081046e5b1b962addf6b03117e6c2e68f00b4592
Makwanahitesh538 <makwanah538@gmail.com>
2025-11-21 10:57:16 +0530

    Update AMI and subnet ID for AWS instance

--- day-3/main.tf
+++ day-3/main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "my" {
   
-     ami           = "ami-01760eea5c574eb86"
+     ami           = "ami-0d176f79571d18a8f"
     instance_type = var.instance_type
     key_name = "mykey" 
-    subnet_id = "subnet-048962092a00fa9de"
+    subnet_id = "subnet-0fe90fae97ec2eb1f"
     
     tags = {
         Name = "MyFirstInstance"
